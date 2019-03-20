6e664eb2361e84f7784e46ff53330331639dfea2
ARIONILDO MENDES ESCOUTO <ALU201523294@uniritter.local>
2019-03-20 20:56:21 -0300

    Initial Commit

---
+++ app/src/main/java/com/ninjamockers/portaria1/MainActivity.java
@@ -0,0 +1,13 @@
+package com.ninjamockers.portaria1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
